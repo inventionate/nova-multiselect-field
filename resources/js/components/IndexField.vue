b35943fba2563b526f52f7c506c9c838b6792df6
Fabian Mundt <f.mundt@inventionate.de>
2022-04-30 11:51:15 +0200

    Work in belongstomanyfield

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -25,7 +25,9 @@
       >
         {{value}}
         {{i}}
-        {{field.value.label}}
+        {{field.options[value]}}
+
+        {{field.options}}
       <!--{{ field.options[i]['label'] }}<span v-if="i+2 != field.value.length">, </span>-->
       </Link>
     </span>
